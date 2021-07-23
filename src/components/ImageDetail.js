08601d9ac4f9bd4dc83348437f6d852060f0bee2
Netapp DPG <netappdpg@Netapps-Air.lan>
2021-07-23 11:55:28 -0700

    reusable components sample

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, StyleSheet, Image, View } from 'react-native';
+
+const ImageDetail = (props) => {
+    return <View>
+    <Image source={props.imageSource} />
+    <Text>{props.title}</Text> 
+    <Text>Image score - {props.imageScore}</Text> 
+    </View>
+  
+};
+
+const styles = StyleSheet.create({
+   
+});
+
+export default ImageDetail;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+const HomeScreen = ({navigation}) => {
+  return <View>
+   <Text style={styles.text}>Hi there!</Text>
+   <Button 
+    onPress  ={() => navigation.navigate('Components')}
+    title="Go to components Demo" />
+
+    <TouchableOpacity onPress  ={() => navigation.navigate('List')}>
+      <Text>Go to List Demo</Text>
+    </TouchableOpacity>
+
+    <TouchableOpacity onPress  ={() => navigation.navigate('Image')}>
+      <Text>Go to Image Screen</Text>
+    </TouchableOpacity>
+  </View>
+  
 };
 
 const styles = StyleSheet.create({
